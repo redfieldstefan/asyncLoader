508689bd71010e14fb3fe1a4368d5c9b6925a013
Stefan Redfield <redfield.stefan@gmail.com>
2015-07-21 21:12:19 -0700

    integrate node-pluploader

--- routes/upload_routes.js
+++ routes/upload_routes.js
@@ -3,13 +3,30 @@
 var bodyParser = require('body-parser');
 var url = require('url');
 var fs = require('fs');
+var Pluploader = require('node-pluploader');
+
+var pluploader = new Pluploader();
+
+var uploadDest = './uploads/'
+
+pluploader.on('fileUploaded', function(file, req) {
+  console.log(file);
+  fs.writeFile(uploadDest + file.name, file.data, function(err) {
+    if(err) {
+      console.log(err);
+    }
+  });
+});
+
+pluploader.on('Error', function(err) {
+  console.log("THIS IS HANDLING THE ERROR " + err);
+});
 
 module.exports = function(router) {
   router.use(bodyParser.json());
 
   router.post('/upload', function(req, res) {
-    console.log(req.body);//Empty
-    console.log(req.files);//Empty
+    pluploader.handleRequest(req, res);
   });
 
 };
