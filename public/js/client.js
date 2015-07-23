490b4d9e7073dff5fa4cf8a0fa16dafcdea7f241
Stefan Redfield <redfield.stefan@gmail.com>
2015-07-22 17:25:15 -0700

    multiple uploaders

--- public/js/client.js
+++ public/js/client.js
@@ -9,28 +9,79 @@ var uploader = new plupload.Uploader({
 
 uploader.init();
 
+var uploaders = [];
+
 uploader.bind('FilesAdded', function(up, files) {
-  var html = '';
-  plupload.each(files, function(file) {
-    html += '<li id="' + file.id + '">' + file.name + ' (' + plupload.formatSize(file.size) + ') <b></b></li>';
-  });
-  document.getElementById('fileList').innerHTML += html;
-  document.getElementById('queued').innerHTML = 'Files Queued: ' + uploader.total.queued;
+  for(var i = 0; i<files.length; i++) {
+    var newUploader = new plupload.Uploader({
+      browse_button: 'hidden',
+      url: '/upload',
+    });
+    newUploader.init();
+    newUploader.files.push(files[i]);
+    uploaders.push(newUploader);
+    uploader.files.pop();
+  };
+  configUploaders();
 });
 
-uploader.bind('UploadProgress', function(up, file) {
-  document.getElementById(file.id).getElementsByTagName('b')[0].innerHTML = '<span>' + file.percent + "%</span>";
-});
+var configUploaders = function() {
+  for(var i = 0; i < uploaders.length; i++) {
+    var html = '';
+    html += '<li id="' + uploaders[i].files[0].id + '">' + uploaders[i].files[0].name + ' (' + plupload.formatSize(uploaders[i].files[0].size) + ') <b></b></li>';
+    document.getElementById('fileList').innerHTML += html;
 
-uploader.bind('StateChanged', function(up) {
-  document.getElementById('queued').innerHTML = 'Files Queued: ' + uploader.total.queued;
-  document.getElementById('uploaded').innerHTML = 'uploaded: ' + uploader.total.uploaded;
-});
+    uploaders[i].bind('UploadProgress', function(up, file) {
+      document.getElementById(file.id).getElementsByTagName('b')[0].innerHTML = '<span>' + file.percent + "%</span>";
+    });
+
+    uploaders[i].bind('StateChanged', function(up) {
+      document.getElementById('queued').innerHTML = 'Files Queued: ' + uploader.total.queued;
+      document.getElementById('uploaded').innerHTML = 'uploaded: ' + uploader.total.uploaded;
+    });
+
+    uploaders[i].bind('Error', function(up, err) {
+      document.getElementById('console').innerHTML += "\nError #" + err.code + ": " + err.message;
+    });
+  };
+};
 
 uploader.bind('Error', function(up, err) {
-  document.getElementById('console').innerHTML += "\nError #" + err.code + ": " + err.message;
-});
+      document.getElementById('console').innerHTML += "\nError #" + err.code + ": " + err.message;
+    });
 
 document.getElementById('start-upload').onclick = function() {
-  uploader.start();
+  for(var i = 0; i < uploaders.length; i ++) {
+    uploaders[i].start();
+  }
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// uploader.bind('UploadProgress', function(up, file) {
+//   document.getElementById(file.id).getElementsByTagName('b')[0].innerHTML = '<span>' + file.percent + "%</span>";
+// });
+
+// uploader.bind('StateChanged', function(up) {
+//   document.getElementById('queued').innerHTML = 'Files Queued: ' + uploader.total.queued;
+//   document.getElementById('uploaded').innerHTML = 'uploaded: ' + uploader.total.uploaded;
+// });
+
+// uploader.bind('Error', function(up, err) {
+//   document.getElementById('console').innerHTML += "\nError #" + err.code + ": " + err.message;
+// });
+
+// document.getElementById('start-upload').onclick = function() {
+//   uploader.start();
+// };
