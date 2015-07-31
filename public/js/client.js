77ce6efa7745e5b4233e486eb9b764428c40e7ac
Stefan Redfield <redfield.stefan@gmail.com>
2015-07-30 17:00:40 -0700

    only assign files and start uploading resting workers

--- public/js/client.js
+++ public/js/client.js
@@ -1,153 +1,89 @@
-'use strict';
+"use strict";
 
 var masterUploader = new plupload.Uploader({
-    browse_button: 'browseFiles',
-    url: '/upload',
+    browse_button: "browseFiles",
+    url: "/upload",
     dragdrop: true,
-    drop_element: 'drop'
+    drop_element: "drop"
 });
 
 masterUploader.init();
 
 var filesUploading = 0;
 var filesUploaded = 0;
-var workerCount = document.getElementById('workerCount').value;
-var workerIndex = 0;
 var uploaders = [];
 
-masterUploader.bind('FilesAdded', function(up, files) {
-    var workerCount = document.getElementById('workerCount').value;
+masterUploader.bind("FilesAdded", function(up, files) {
+    var workerCount = document.getElementById("workerCount").value;
     if(uploaders.length === 0) {
-        buildWorkers(workerCount);
+        build_Workers(workerCount);
     }
     for(var i = 0; i < files.length; i++ ){
-        var html = '';
-        html += '<li>' + files[i].name + ' (' + plupload.formatSize(files[i].size) + ') <b></b></li>';
-        document.getElementById('fileList').innerHTML += html;
+        var html = "";
+        html += "<li>" + files[i].name + " (" + plupload.formatSize(files[i].size) + ") <b></b></li>";
+        document.getElementById("fileList").innerHTML += html;
     }
-  //for(var i = 0; i < files.length; i++) {
-  //  if(workerIndex === workerCount) {
-  //    workerIndex = 0;
-  //  }
-  //  if(!uploaders[workerIndex]){
-  //    workerIndex = 0;
-  //  }
-  //  console.log(masterUploader.files)
-  //  var currentWorker = uploaders[workerIndex];
-  //  currentWorker.files.push(files[i]);
-  //  displayFile(currentWorker, 'fileList');
-  //  filesUploading++;
-  //  workerIndex++;
-  //}
 });
 
-var buildWorkers = function(count) {
+var build_Workers = function(count) {
     for(var i = 0; i < count; i++) {
         var newUploader = new plupload.Uploader({
-            browse_button: 'hidden',
-            url: '/upload'
+            browse_button: "hidden",
+            url: "/upload"
         });
         newUploader.init();
-        newUploader.uploading = false;
         configUploader(newUploader);
         uploaders.push(newUploader);
     };
 };
 
-var displayFile = function(worker,listID) {
+var start_uploader = function(uploader){
+    uploader.files.push(masterUploader.files.shift());
+    display_File(uploader, "uploadList");
+    uploader.start();
+    filesUploading++;
+}
+
+var display_File = function(worker,listID) {
     var newFile = (worker.files.length - 1);
-    var html = '';
-    html += '<li id="' + worker.files[newFile].id + '">' + worker.files[newFile].name + ' (' + plupload.formatSize(worker.files[newFile].size) + ') <b></b></li>';
+    var html = "";
+    html += "<li id='" + worker.files[newFile].id + "'>" + worker.files[newFile].name + " (" + plupload.formatSize(worker.files[newFile].size) + ") <b></b></li>";
     document.getElementById(listID).innerHTML += html;
 }
 
 var configUploader = function(newUploader) {
 
-    newUploader.bind('UploadProgress', function(up, file) {
-        document.getElementById(file.id).getElementsByTagName('b')[0].innerHTML = '<span>' + file.percent + "%</span>";
-        if(file.percent === 100){
-            up.uploading = false;
-        }
+    newUploader.bind("UploadProgress", function(up, file) {
+        document.getElementById(file.id).getElementsByTagName("b")[0].innerHTML = "<span>" + file.percent + "%</span>";
     });
 
-    newUploader.bind('FileUploaded', function(up) {
+    newUploader.bind("FileUploaded", function(up) {
         filesUploaded ++;
         filesUploading --;
-        up.uploading = false;
-        document.getElementById('uploaded').innerHTML = 'files uploaded: <em>' + filesUploaded + '</em>';
-        document.getElementById('uploading').innerHTML = 'files uploading: <em> ' + filesUploading + '</em>';
+        document.getElementById("uploaded").innerHTML = "files uploaded: <em>" + filesUploaded + "</em>";
+        document.getElementById("uploading").innerHTML = "files uploading: <em> " + filesUploading + "</em>";
     });
 
-    newUploader.bind('UploadComplete', function(up) {
+    newUploader.bind("UploadComplete", function(up) {
         if(masterUploader.files.length) {
-            up.files.push(masterUploader.files.shift());
-            displayFile(up, 'uploadList');
-            up.start();
-            filesUploading++;
-        }
-        else{
-         console.log('done');
+            start_uploader(up);
         }
     });
 
-    newUploader.bind('Error', function(up, err) {
-        document.getElementById('console').innerHTML += "\nError #" + err.code + ": " + err.message;
+    newUploader.bind("Error", function(up, err) {
+        document.getElementById("console").innerHTML += "\nError #" + err.code + ": " + err.message;
     });
 };
 
-masterUploader.bind('Error', function(up, err) {
-    document.getElementById('console').innerHTML += "\nError #" + err.code + ": " + err.message;
+masterUploader.bind("Error", function(up, err) {
+    document.getElementById("console").innerHTML += "\nError #" + err.code + ": " + err.message;
 });
 
-var isWorking = function(index) {
-    if(index >= uploaders.length) {
-        index = 0;
-    }
-    if(uploaders[index].uploading){
-        uploaders[index].uploading;
-        //console.log(index);
-        return isWorking(index + 1);
-    }
-    else {
-        return uploaders[index];
-    }
-};
-
-document.getElementById('start-upload').onclick = function() {
-
+document.getElementById("start-upload").onclick = function() {
 
     for(var i = 0; i < uploaders.length; i++){
-        uploaders[i].files.push(masterUploader.files.shift());
-        displayFile(uploaders[i], 'uploadList');
-        uploaders[i].start();
-        filesUploading++;
+        start_uploader(uploaders[i]);
     }
-    //while(masterUploader.files.length){
-    //  if(workerIndex === workerCount) {
-    //        workerIndex = 0;
-    //    }
-    //    if(!uploaders[workerIndex]){
-    //        workerIndex = 0;
-    //    }
-    //    var currentWorker = uploaders[workerIndex];
-    //    if(currentWorker.total.uploaded < currentWorker.files.length) {
-    //        console.log('skip');
-    //        workerIndex++;
-    //    }
-    //    else {
-    //        currentWorker.files.push(masterUploader.files.shift());
-    //        displayFile(currentWorker, 'uploadList');
-    //        currentWorker.start();
-    //        currentWorker.uploading = true;
-    //        filesUploading++;
-    //        workerIndex++;
-    //    }
-    //}
-
-  //for(var i = 0; i < uploaders.length; i ++) {
-  //  document.getElementById('uploading').innerHTML = 'files uploading: ' + filesUploading;
-  //  uploaders[i].start();
-  //  console.log(uploaders[i]);
-  //}
 };
+
 
